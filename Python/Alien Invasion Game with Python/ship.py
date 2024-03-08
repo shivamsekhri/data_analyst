fea21a85feebd84f9314589a41651bdcec1f2a9d
Shivam Sekhri <shivamsekhri91@gmail.com>
2024-03-08 16:20:15 -0500

    Game gif

--- Python/Alien Invasion Game with Python/ship.py
+++ Python/Alien Invasion Game with Python/ship.py
@@ -12,7 +12,7 @@ class Ship(Sprite):
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
-        self.image = pygame.image.load(r"C:\Users\shiva\Desktop\alien_invasion\images\ship.bmp")
+        self.image = pygame.image.load(r"images\ship.bmp")
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen
